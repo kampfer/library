058c2f9e44986710445733333f31feb1ece3fdca
kampfer <l.w.kampfer@gmail.com>
2011-11-12 22:58:15 +0800

    添加sizzle

--- Utilities/utilities.js
+++ Utilities/utilities.js
@@ -1,10 +1,26 @@
-/**
- * 提供常用的工具函数
- * @module utilities
- * @submodule base.library
- * @author l.w.kampfer@gmail.com
+/*
+ *	kampfer - 组件 - 公用函数
+ *	@author l.w.kampfer@gmail.com
+ *	@depend : core.js
  */
 
 (function( kampfer, window ) {
-	
+	kampfer.extend( kampfer, {
+		isObject : function( obj ) {
+			return obj && Object.prototype.toString.call(obj)==='[object Object]' && 'isPrototypeOf' in obj;
+		},
+		isArray : function( obj ) {
+			return obj && Object.prototype.toString.call(obj) === "[object Array]";
+		},
+		isWindow : function( obj ) {
+			return obj && typeof obj === "object" && "setInterval" in obj;
+		},
+		trim : function( str ) {
+			var trimLeft = /^\s+/,
+				trimRight = /\s+$/;
+			return str === null ?
+			'' :
+			str.toString().replace( trimLeft, '' ).replace( trimRight, '' );
+		}	
+	});
 })( kampfer, window );
